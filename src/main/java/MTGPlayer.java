19d0bd1b70344e1b5c5aa2717ceb315e447b3075
Conner Gleason <connereg@optonline.net>
2022-06-27 13:48:59 -0400

    Testing out complex classes

---
+++ src/main/java/MTGPlayer.java
@@ -0,0 +1,52 @@
+public class MTGPlayer {
+    private String playerName;
+    private Boolean hasADeck;
+    private Integer playerAge;
+    private String favoriteCardColor;
+    private String hasFavoriteFormat;
+
+    public MTGPlayer(String playerName, Boolean hasADeck, int playerAge, String favoriteCardColor, String hasFavoriteFormat) {
+        this.playerName = playerName;
+        this.hasADeck = hasADeck;
+        this.playerAge = playerAge;
+        this.favoriteCardColor = favoriteCardColor;
+        this.hasFavoriteFormat = hasFavoriteFormat;
+
+    }
+
+    public String introducePlayer() {
+        String intro = "Hello, my name is " + this.playerName;
+        return intro;
+    }
+
+    public String doesPlayerOwnDeck() {
+        String playerResponse;
+        if (this.hasADeck == true) {
+            playerResponse = "Yes, " + this.playerName + " has a deck to play with";
+        }
+        else {
+            playerResponse = "No, " + this.playerName + " does NOT have a deck to play with";
+        }
+
+        return playerResponse;
+    }
+
+    public String whatIsPlayersAge() {
+        String playerResponse;
+        playerResponse = this.playerName + "is " + this.playerAge + "years old";
+        return playerResponse;
+    }
+
+    public String whatIsPlayersFavoriteCardColor() {
+        String playerResponse;
+        playerResponse = this.playerName + "'s favorite color to play in MTG is " + this.favoriteCardColor;
+        return playerResponse;
+    }
+
+    public String whatIsPlayersFavoriteFormat() {
+        String playerResponse;
+        playerResponse = this.playerName + "'s favorite format to play MTG in is " + this.hasFavoriteFormat;
+        return playerResponse;
+    }
+    
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,3 +3,26 @@ public class Main {
         // your code here
     }
 }
+
+//PSEUDOCODE:
+//  Class => MTG Player
+//  MTG Player Standard Props:
+//  playerName = String
+//  hasADeck = Boolean
+//  hasAnAge = Integer
+//  hasFavoriteColor = String
+//  hasFavoriteFormat = DEFAULT STRING
+//
+//  Subclasses of MTG Player => "Timmy", "Johnny", "Spike"
+//
+//  Properties of Timmy MTG Player:
+//  likesBadCards = Boolean
+//  playsBigCreatureCard = METHOD => Plays a Big Creature Card
+//
+//  Propteries of Johnny MTG Player:
+//  likesComboCards = Boolean
+//  playsComboCard = METHOD => Plays a Combo Card
+//
+//  Properties of Spike MTG Player:
+//  likesPowerfulCards = Boolean 
+//  playsPowerfulCard = METHOD => Plays a Powerful Card
